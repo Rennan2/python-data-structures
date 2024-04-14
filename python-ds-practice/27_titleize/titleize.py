b188fd7c58a2082afafc87ddd3be17c2ae82ac0c
Rennan2 <152652735+Rennan2@users.noreply.github.com>
2024-04-14 17:59:03 -0400

    Add files via upload

---
+++ python-ds-practice/27_titleize/titleize.py
@@ -0,0 +1,12 @@
+def titleize(phrase):
+    """Return phrase in title case (each word capitalized).
+
+        >>> titleize('this is awesome')
+        'This Is Awesome'
+
+        >>> titleize('oNLy cAPITALIZe fIRSt')
+        'Only Capitalize First'
+    """
+    word = phrase.lower().split(" ")
+    capitalized_words = [word.capitalize() for word in words]
+    return " ".join(capitalized_words)
